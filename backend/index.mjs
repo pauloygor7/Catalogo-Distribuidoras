c0462f2afa3326473dbab94f127ba4f2f786945f
Paulo Ygor Feitoza <pauloygor700@gmail.com>
2026-05-25 22:03:38 -0300

    configurar conexão inicial com o banco de dados

---
+++ backend/index.mjs
@@ -0,0 +1,19 @@
+import { db } from "./database/db.mjs";
+import { get, ref } from "firebase/database";
+
+try {
+  const dbRef = ref(db, "/ping");
+  const before = Date.now;
+  const snapshot = await get(dbRef);
+  const after = Date.now;
+  const ping = after - before;
+  if (!snapshot.exists()) {
+    console.log("Dado não existente");
+  } else {
+    const data = snapshot.val();
+    console.log("Sucesso ao logar na firebase!");
+    console.log(`${data} ${ping}ms.`);
+  }
+} catch (e) {
+  console.log("Erro ao logar na firebase!");
+}
