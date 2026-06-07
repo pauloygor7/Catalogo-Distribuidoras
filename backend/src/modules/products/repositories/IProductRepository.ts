5b18ff2c69189c809578578a7754028f22bc1ef2
Paulo Ygor Feitoza <pauloygor700@gmail.com>
2026-06-07 00:09:19 -0300

    adiciona método update e integra UpdateProductDTO ao repositório

--- backend/src/modules/products/repositories/IProductRepository.ts
+++ backend/src/modules/products/repositories/IProductRepository.ts
@@ -1,10 +1,12 @@
 import { Product } from '../entities/Product';
 import { CreateProductDTO } from '../dtos/createProduct.dto';
+import { UpdateProductDTO } from '../dtos/updateProduct.dto';
 
 export interface IProductRepository {
     create(data: CreateProductDTO): Promise<Product>;
     findAll(): Promise<Product[]>;
     findById(id: string): Promise<Product | null>;
-    update(id: string, data: CreateProductDTO): Promise<Product>;
+    update(id: string, data: UpdateProductDTO): Promise<Product>;
     delete(id: string): Promise<void>;
+    findByIdCategory(categoriaId: string): Promise<Product[]>;
 }
